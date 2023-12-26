cfe9987503143ad5b8c3cc135a74c1d966572d1a
JojoYay <mobilejoz@gmail.com>
2023-12-26 06:47:24 +0000

    video path problem with Win test

--- src/animatediff/test.py
+++ src/animatediff/test.py
@@ -5,4 +5,4 @@ from pathlib import Path
 movie_path=Path("data\video\dance00001.mp4")
 movie_path = movie_path.resolve()
 print(f"movie_path.resolve(): {movie_path}")
-probe = ffmpeg.probe()
+probe = ffmpeg.probe(movie_path)
