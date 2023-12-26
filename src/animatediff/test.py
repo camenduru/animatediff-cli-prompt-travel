0512d96ea14dc4ee1f79c09ea9bd10d6f7af3175
JojoYay <mobilejoz@gmail.com>
2023-12-26 06:59:00 +0000

    video path problem with Win test

--- src/animatediff/test.py
+++ src/animatediff/test.py
@@ -1,8 +1,10 @@
 import ffmpeg
 import json
 from pathlib import Path
+import re
 
-movie_path=Path("data\\video\\dance00001.mp4")
+movie_path=Path("data\video\dance00001.mp4")
 movie_path = movie_path.resolve()
-print(f"movie_path.resolve(): {movie_path}")
-probe = ffmpeg.probe(movie_path)
+escaped = re.escape(movie_path)
+print(f"movie_path.resolve(): {escaped}")
+probe = ffmpeg.probe(escaped)
