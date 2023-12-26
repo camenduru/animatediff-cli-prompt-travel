520142ab83e1c2fdfb511a7e85693248474baf83
JojoYay <mobilejoz@gmail.com>
2023-12-26 07:26:57 +0000

    update

--- src/animatediff/test.py
+++ src/animatediff/test.py
@@ -4,6 +4,6 @@ from pathlib import Path
 import re
 
 
-movie_path=Path(r"data\video\dance00001.mp4")
+movie_path=Path(r"data\video\dance00001.mp4").resolve()
 print(f"movie_path.resolve(): {movie_path}")
 probe = ffmpeg.probe(movie_path)
