3a4cbc37349577a342d97921a3c4f81061fcd96c
JojoYay <mobilejoz@gmail.com>
2023-12-26 07:15:59 +0000

    fuck windows

--- src/animatediff/test.py
+++ src/animatediff/test.py
@@ -3,8 +3,7 @@ import json
 from pathlib import Path
 import re
 
-movie_path=Path("data\video\dance00001.mp4")
-movie_path = movie_path.resolve()
-escaped = re.escape(movie_path)
-print(f"movie_path.resolve(): {escaped}")
-probe = ffmpeg.probe(escaped)
+
+movie_path=Path(r"data\video\dance00001.mp4")
+print(f"movie_path.resolve(): {movie_path}")
+probe = ffmpeg.probe(movie_path)
